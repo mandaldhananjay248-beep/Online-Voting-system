7daf68ecd842aaa790e8673282137d1441d297eb
mandaldhananjay248-beep <mandaldhananjay248@gmail.com>
2025-10-30 10:00:10 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,9 +1,12 @@
 import json
-import bcrypt
+import hashlib
 import streamlit as st
+import os
 
 class AuthSystem:
-    def __init__(self, users_file='data/users.json'):
+    def __init__(self, users_file=None):
+        if users_file is None:
+            users_file = os.path.join(os.path.dirname(__file__), 'data', 'users.json')
         self.users_file = users_file
         self.load_users()
     
@@ -13,18 +16,23 @@ class AuthSystem:
                 self.users = json.load(f)
         except FileNotFoundError:
             self.users = {}
+            self.save_users()
     
     def save_users(self):
         with open(self.users_file, 'w') as f:
             json.dump(self.users, f, indent=4)
     
+    def _hash_password(self, password):
+        """Simple password hashing using SHA-256"""
+        return hashlib.sha256(password.encode('utf-8')).hexdigest()
+    
     def register_user(self, username, password):
         if username in self.users:
             return False, "Username already exists"
         
-        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        hashed_password = self._hash_password(password)
         self.users[username] = {
-            'password': hashed_password.decode('utf-8'),
+            'password': hashed_password,
             'voted': False
         }
         self.save_users()
@@ -34,14 +42,15 @@ class AuthSystem:
         if username not in self.users:
             return False, "User not found"
         
-        stored_hash = self.users[username]['password'].encode('utf-8')
-        if bcrypt.checkpw(password.encode('utf-8'), stored_hash):
+        hashed_password = self._hash_password(password)
+        if self.users[username]['password'] == hashed_password:
             return True, "Login successful"
         return False, "Invalid password"
     
     def has_voted(self, username):
-        return self.users[username].get('voted', False)
+        return self.users.get(username, {}).get('voted', False)
     
     def mark_voted(self, username):
-        self.users[username]['voted'] = True
-        self.save_users()
+        if username in self.users:
+            self.users[username]['voted'] = True
+            self.save_users()
